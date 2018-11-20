4cb5a381fd315140b51eb6745f2b4d5c10910ebe
Aaron <aaron.winkler98@googlemail.com>
2018-11-20 02:08:06 +0100

    ready v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
 #include <Arduino.h>
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+// PINs
+int pir_sensor = 2;   //PWM pin 2
+
+//433MHz codes
+int motion_code = 13100;
+
+//void declaration
+void motion_detected();
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("setup begin");
+  pinMode(pir_sensor, INPUT);
+  mySwitch.enableTransmit(3);
+  attachInterrupt(digitalPinToInterrupt(2), motion_detected, RISING);
+
+
+  Serial.println("setup finished");
 }
 
 void loop() {
-  Serial.println("Debug-Test");
-  delay(1000);
-  Serial.println("Done");
-  delay(1000);
 
-  Serial.println("This is for you, GitHub :D");
+}
 
+void motion_detected(){
+  mySwitch.send(motion_code, 24);
+  Serial.println("motion detected");
 }
